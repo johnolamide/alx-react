40c43f3dff237a3948f133bd5c0e14f83e2f01b9
Olamide Alokan <alokanolamide@gmail.com>
2024-01-31 11:02:38 +0100

    modified task_2

--- 0x01-react_intro/task_1/dashboard/src/App.js
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -11,12 +11,14 @@ function App() {
 				<h1>School dashboard</h1>
 			</header>
 			<body className="App-body">
-				<p>Login to access the full dashboard</p>
-				<label htmlFor="email">Email:</label>
-				<input type="email" id="email" name="email" />
-				<label htmlFor="password">Password:</label>
-				<input type="password" id="password" name="password" />
-				<button>OK</button>
+        <p>Login to access the full dashboard</p>
+        <div className="Login">
+				  <label htmlFor="email">Email:</label>
+				  <input type="email" id="email" name="email" />
+          <label htmlFor="password">Password:</label>
+				  <input type="password" id="password" name="password" />
+				  <button>OK</button>
+        </div>
 			</body>
 			<footer className="App-footer">
 				<p>
--- 0x01-react_intro/task_1/dashboard/src/index.js
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -7,12 +7,12 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-    <div className='root-notifications'>
-      <Notifications />
-    </div>
-  </React.StrictMode>
+	<React.StrictMode>
+		<div className="root-notifications">
+			<Notifications />
+		</div>
+		<App />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
