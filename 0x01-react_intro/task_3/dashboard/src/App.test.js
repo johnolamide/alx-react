125b0bee2346bc775399a9b097e8a154bd344b49
Olamide Alokan <alokanolamide@gmail.com>
2024-01-31 19:22:12 +0100

    updated task_3

---
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('renders App-header', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.App-header').exists()).toBe(true);
+  });
+
+  it('renders App-body', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.App-body').exists()).toBe(true);
+  });
+
+  it("renders App-footer", () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find(".App-footer").exists()).toBe(true);
+  });
+});
