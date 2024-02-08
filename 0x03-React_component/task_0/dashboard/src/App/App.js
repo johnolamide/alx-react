ce04e93fcc1913d82b110c2eb22de4b4d88d876b
Olamide Alokan <alokanolamide@gmail.com>
2024-02-08 20:14:45 +0100

    [ADD] task_0

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -43,34 +43,4 @@ class App extends React.Component {
   }
 }
 
-// const App = ({ isLoggedIn = false }) => {
-// 	let listCourses = [
-// 	  {id: 1, name: 'ES6', credit: 60},
-// 	  {id: 2, name: 'Webpack', credit: 20},
-// 	  {id: 3, name: 'React', credit: 40},
-// 	];
-
-// 	let listNotifications = [
-// 	  {id: 1, type: "default", value: "New course available"},
-// 	  {id: 2, type: "urgent", value: "New resume available"},
-// 	  {id: 3, type: "urgent", html: { __html: getLatestNotification() }},
-// 	];
-
-// 	return (
-// 		<>
-// 			<Notifications listNotifications={listNotifications}/>
-// 			<div className="App">
-// 				<Header />
-// 				<div className="App-body">
-// 					{isLoggedIn
-// 						? <CourseList listCourses={listCourses} />
-// 						: <Login />
-// 					}
-// 				</div>
-// 				<Footer />
-// 			</div>
-// 		</>
-// 	);
-// }
-
 export default App;
