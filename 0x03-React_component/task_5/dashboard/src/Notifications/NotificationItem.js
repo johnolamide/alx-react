2a3aabd6d09e8483740808d08fca51e6f0858521
Olamide Alokan <alokanolamide@gmail.com>
2024-02-12 15:35:02 +0100

    [ADD] task_5

---
+++ 0x03-React_component/task_5/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class NotificationItem extends React.PureComponent {
+  render() {
+    const { type, html, value, markAsRead, id } = this.props;
+    let li;
+
+    value
+      ? (li = <li data-notification-type={type} onClick={() => markAsRead(id)}>{value}</li>)
+      : (li = (
+        <li data-notification-type={type} dangerouslySetInnerHTML={html} onClick={() => markAsRead(id)}></li>
+      ));
+    return li;
+  }
+}
+
+NotificationItem.defaultProps = {
+  id: 0,
+  type: 'default',
+  value: '',
+  html: {},
+  markAsRead: () => { },
+};
+
+NotificationItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  type: PropTypes.string,
+  html: PropTypes.shape({ __html: PropTypes.string }),
+  value: PropTypes.string,
+  markAsRead: PropTypes.func,
+};
+
+export default NotificationItem;
