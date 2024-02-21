89cc0e3d928684af2a6a06a471fcda15dfebd0f3
Olamide Alokan <alokanolamide@gmail.com>
2024-02-21 14:33:11 +0100

    [ADD] 0-fromjs.js

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -3,14 +3,3 @@ const Immutable = require('immutable');
 function getImmutableObject(obj) {
   return Immutable.fromJS(obj);
 }
-
-let obj = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132,
-};
-
-let immutableObj = getImmutableObject(obj);
-
-console.log(immutableObj);
