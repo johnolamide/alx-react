d9a0b4d6c54f47b2f5d1a10804836563a0aea0be
John Olamide <45767771+johnolamide@users.noreply.github.com>
2024-04-10 12:23:44 +0100

    Delete 0x08-react_redux_reducer_selector directory

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/schema/courses.js
+++
@@ -1,6 +0,0 @@
-import { normalize, schema } from "normalizr";
-
-export const courseSchema = new schema.Entity('courses');
-export const coursesNormalizer = (data) => {
-  return normalize(data, [courseSchema]);
-}
