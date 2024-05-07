50a78d6619f4a6751899bcbf73b6fc57f3808316
Olamide Alokan <alokanolamide@gmail.com>
2024-05-07 17:48:06 +0100

    [ADD] task_3

---
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/notificationReducer.js
@@ -0,0 +1,34 @@
+import { FETCH_NOTIFICATION_SUCCESS, MARK_AS_READ, SET_TYPE_FILTER } from "../actions/notificationActionTypes";
+
+const initialState = {
+  notifications: [],
+  filter: 'DEFAULT',
+};
+
+const notificationReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_NOTIFICATION_SUCCESS:
+      return {
+        ...state,
+        notifications: action.data.map(notification => ({
+          ...notification, isRead: false
+        }))
+      };
+    case MARK_AS_READ:
+      return {
+        ...state,
+        notifications: state.notifications.map(notification => notification.id === action.index
+          ? { ...notification, isRead: true }
+          : notification)
+      };
+    case SET_TYPE_FILTER:
+      return {
+        ...state,
+        filter: action.filter
+      };
+    default:
+      return state;
+  }
+};
+
+export default notificationReducer;
