3261e514c392df263d45e71b75e072ca7dd1ba27
Olamide Alokan <alokanolamide@gmail.com>
2024-05-09 19:32:08 +0100

    [ADD] task_2

---
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Header/Header.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { connect } from 'react-redux';
+import { logout } from '../actions/uiActionCreators';
+import { StyleSheet, css } from "aphrodite";
+import AppContext from "../App/AppContext";
+import logo from "../assets/holberton-logo.jpg";
+
+const styles = StyleSheet.create({
+	appHeader: {
+		display: "flex",
+		position: "sticky",
+		top: "0",
+		width: "100%",
+		borderBottom: "0.2rem solid #D73953",
+		color: "#D73953",
+		alignItems: "center",
+	},
+	appHeaderImg: {
+		height: "200px",
+		width: "200px",
+	},
+});
+
+const mapStateToProps = state => {
+	return {
+		user: state.uiReducer.user
+	};
+};
+
+class Header extends React.Component{
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<AppContext.Consumer>
+				{({ user, logOut }) => (
+					<>
+						<header className={css(styles.appHeader)}>
+							<img
+								src={logo}
+								alt="Holberton logo"
+								className={css(styles.appHeaderImg)}
+							/>
+							<h1>School dashboard</h1>
+						</header>
+						{user.isLoggedIn && (
+							<div id="logoutSection">
+								<p>Welcome {user.email} (<span onClick={logOut} style={{cursor: "pointer"}}>logout</span>)</p>
+							</div>
+						)}
+					</>
+				)}
+			</AppContext.Consumer>
+		);
+	}
+}
+
+export default connect(mapStateToProps, { logout })(Header);
