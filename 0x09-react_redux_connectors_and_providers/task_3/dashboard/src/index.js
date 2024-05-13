d654b2a43a7a0f01d65868c88765cf715e03b464
Olamide Alokan <alokanolamide@gmail.com>
2024-05-13 18:53:34 +0100

    [ADD] task_3

---
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App/App';
+import reportWebVitals from './reportWebVitals';
+import { uiReducer } from './reducers/uiReducer';
+
+const store = createStore(uiReducer, applyMiddleware(thunk));
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
+);
+reportWebVitals();
